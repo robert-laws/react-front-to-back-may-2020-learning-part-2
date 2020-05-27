e8d725596e7612a577408ce77dcf542c794e5f95
Robert Laws <roblaws@me.com>
2020-05-27 17:07:46 +0300

    add express routes

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact keeper api' }))
 
+// define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
