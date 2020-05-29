9bed2da022c08788e0d0686a967ad0b214907efe
Robert Laws <roblaws@me.com>
2020-05-29 09:09:13 +0300

    setup basic user creation

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check')
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator')
 
 const User = require('../models/User');
 
@@ -13,7 +14,7 @@ router.post('/', [
   check('password', 'Please enter a password with 6 or more characters').isLength({
     min: 6
   })
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({
@@ -23,7 +24,29 @@ router.post('/', [
     // res.send(req.body);
   }
 
-  res.send('passed')
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if(user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    user = new User({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    res.send('User saved')
+  } catch(error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+
 });
 
 module.exports = router;
