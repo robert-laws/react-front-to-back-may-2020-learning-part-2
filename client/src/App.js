c45957a7c9c8d70badd672383ac75347c7b8ae49
Robert Laws <roblaws@me.com>
2020-05-31 08:11:44 +0300

    begin auth context setup

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,28 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
     <>
-      <ContactState>
-        <Router>
-          <NavigationBar />
-          <Container>
-            <Row>
-              <Col>
-                <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                </Switch>
-              </Col>
-            </Row>
-          </Container>
-        </Router>
-      </ContactState>
+      <AuthState>
+        <ContactState>
+          <Router>
+            <NavigationBar />
+            <Container>
+              <Row>
+                <Col>
+                  <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/about' component={About} />
+                  </Switch>
+                </Col>
+              </Row>
+            </Container>
+          </Router>
+        </ContactState>
+      </AuthState>
     </>
   );
 }
--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,3 +1,4 @@
+// Contact Types
 export const ADD_CONTACT = 'ADD_CONTACT';
 export const DELETE_CONTACT = 'DELETE_CONTACT';
 export const SET_CURRENT = 'SET_CURRENT';
@@ -7,3 +8,13 @@ export const FILTER_CONTACTS = 'FILTER_CONTACTS';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+// Auth Types
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'REGISTER';
+export const LOGOUT = 'LOGOUT';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
